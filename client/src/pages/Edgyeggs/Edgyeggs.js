e4df9b61c3a778e47905ef7e145c1c65a3165017
Wollivan <tim.smith2814@gmail.com>
2022-11-17 00:04:21 +0000

    Add counts to each rarity

--- client/src/pages/Edgyeggs/Edgyeggs.js
+++ client/src/pages/Edgyeggs/Edgyeggs.js
@@ -3,20 +3,88 @@ import "./Edgyeggs.css";
 import { useEffect, useState } from "react";
 import edgyegg from "../../assets/logo-white.svg";
 import SelectedEdgyegg from "../../components/SelectedEdgyEgg/SelectedEdgyegg";
+// import axios from "axios";
+import mintedEggs from "../../mintedeggs.json";
 
 export default function Edgyeggs({ getSearchEdgyegg, handleSearchEdgyegg, setpageClass, formEdgyeggSearch, edgyeggSearch }) {
   useEffect(() => {
     setpageClass("edgyeggs");
+    getMintedRarities();
   }, []);
 
   const [showModalEdgyegg, setshowModaEdgyegg] = useState(false);
 
   const [activeEdgyegg, setactiveEdgyegg] = useState({});
 
+  const [mintedRarities, setmintedRarities] = useState({
+    Mythic: 0,
+    Leggendary: 0,
+    Eggotic: 0,
+    Eggceptional: 0,
+    Eggstraordinary: 0,
+    Eggcelent: 0,
+    Eggspendable: 0,
+  });
+
   const handleModalEdgyegg = (e) => {
     e.preventDefault();
     setshowModaEdgyegg(!showModalEdgyegg);
     setactiveEdgyegg({ ...edgyeggSearch });
+  };
+
+  const getMintedRarities = async () => {
+    // const API = ``; // here will be the api call that gets a list of all minted eggs and their rarities
+    // const res = await axios.get(API);
+
+    // count the occurance of each rarity
+    let Mythic = 0;
+    let Leggendary = 0;
+    let Eggotic = 0;
+    let Eggceptional = 0;
+    let Eggstraordinary = 0;
+    let Eggcelent = 0;
+    let Eggspendable = 0;
+
+    // here we loop through the data and increment each rarity count when it matches (this method only works because we already know the property names)
+    // res.data.forEach((egg) => { // we want to use the response from an API call, but we don't have it yet
+    mintedEggs.forEach((egg) => {
+      // so we just use the json file Tim scraped together
+      switch (egg.Rarity) {
+        case "Mythic":
+          Mythic++;
+          break;
+        case "Leggendary":
+          Leggendary++;
+          break;
+        case "Eggotic":
+          Eggotic++;
+          break;
+        case "Eggceptional":
+          Eggceptional++;
+          break;
+        case "Eggstraordinary":
+          Eggstraordinary++;
+          break;
+        case "Eggcelent":
+          Eggcelent++;
+          break;
+        case "Eggspendable":
+          Eggspendable++;
+          break;
+        default:
+          break;
+      }
+    });
+    // set state to be the counts
+    setmintedRarities({
+      Mythic,
+      Leggendary,
+      Eggotic,
+      Eggceptional,
+      Eggstraordinary,
+      Eggcelent,
+      Eggspendable,
+    });
   };
 
   return (
@@ -63,13 +131,13 @@ export default function Edgyeggs({ getSearchEdgyegg, handleSearchEdgyegg, setpag
           <h2>Raritys</h2>
         </div>
         <div className="bottomContainer-edgyegg">
-          <p>Mythic:20</p>
-          <p>Leggendary:80 </p>
-          <p>Eggotic:500 </p>
-          <p>Eggceptional:900 </p>
-          <p>Eggstraordinary:1600 </p>
-          <p>Eggcellent:2500 </p>
-          <p>Eggspendable:4400 </p>
+          <p>Mythic: {mintedRarities.Mythic}/20</p>
+          <p>Leggendary: {mintedRarities.Leggendary}/80 </p>
+          <p>Eggotic: {mintedRarities.Eggotic}/500 </p>
+          <p>Eggceptional: {mintedRarities.Eggceptional}/900 </p>
+          <p>Eggstraordinary: {mintedRarities.Eggstraordinary}/1600 </p>
+          <p>Eggcelent: {mintedRarities.Eggcelent}/2500 </p>
+          <p>Eggspendable: {mintedRarities.Eggspendable}/4400 </p>
         </div>
       </div>
     </>
